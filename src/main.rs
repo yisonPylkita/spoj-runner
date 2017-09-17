4fb115ce769d37c7ef262fafb716dcdbba101622
yisonPylkita <yisonPylkita@gmail.com>
2017-09-17 21:55:40 +0000

    Some info about project

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+/// spoj-runner - automate testing your programs for SPOJ
+///
+/// Usage:
+///   Run this application in root directory of your project.
+///   It have to contain sub-directory 'tests' where each 
+///   subdirectory is a black-box test of your application.
+///   These directories has to contains two files: in.txt 
+///   and out.txt. in.txt contains input to your application.
+///   out.txt contains expected result. spoj-runner automates 
+///   boring manual testing by running given tests and shows 
+///   tests status in nicely formatted way.
+
+
+fn main() {
+    println!("Hello, world!");
+}
